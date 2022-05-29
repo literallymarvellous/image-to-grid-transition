b9df0c88f141f4ed23cbf3c2912eb064571e720f
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-05-29 12:08:07 +0100

    finished home

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ import gsap from "gsap";
 import Flip from "gsap/Flip";
 
 gsap.registerPlugin(Flip);
+
+const enterBtn = document.querySelector(".home__item-image__btn");
+const backBtn = document.querySelector(".btn-back");
+const gridConatiner = document.querySelector(".grid__conatiner");
+const home = document.querySelector(".home");
+console.log(enterBtn);
+
+enterBtn.addEventListener("click", () => {});
