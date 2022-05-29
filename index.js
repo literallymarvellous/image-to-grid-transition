811d1d4be589a9aefd8498875ee8e5887a581a7c
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-05-29 12:46:35 +0100

    finished grid

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const enterBtn = document.querySelector(".home__item-image__btn");
 const backBtn = document.querySelector(".btn-back");
 const gridConatiner = document.querySelector(".grid__conatiner");
 const home = document.querySelector(".home");
-console.log(enterBtn);
+console.log(home);
 
-enterBtn.addEventListener("click", () => {});
+enterBtn.addEventListener("click", () => {
+  home.classList.add("nodisplay");
+  gridConatiner.classList.add("grid--open");
+  console.log("hey");
+});
+
+backBtn.addEventListener("click", () => {
+  gridConatiner.classList.remove("no-display");
+  home.classList.remove("grid--open");
+});
--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,6 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 module.exports = {
   plugins: [
     require("autoprefixer"),
-    purgecss({
-      content: ["./**/*.html"],
-    }),
     require("postcss-import"),
     require("postcss-url"),
     require("postcss-custom-properties"),
